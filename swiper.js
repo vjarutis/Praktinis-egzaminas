65474b3fabcfea59e331874f2ec2ea365759876c
vjarutis <vjarutis@gmail.com>
2023-05-07 12:01:56 +0300

    swiper

---
+++ swiper.js
@@ -0,0 +1,36 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 3,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+
+    breakpoints: {
+        1000: {
+            slidesPerView: 3,
+        },
+
+        600: {
+            slidesPerView: 2,
+        },
+
+        320: {
+            slidesPerView: 1,
+        },
+    },
+  });
